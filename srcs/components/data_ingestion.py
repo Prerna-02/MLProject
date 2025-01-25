449f4c81b41f416ceeab1f71a34e86b033c759d3
Prerna <prernatemp17@gmail.com>
2025-01-26 00:34:54 +0530

    Model Trainer

--- srcs/components/data_ingestion.py
+++ srcs/components/data_ingestion.py
@@ -11,6 +11,12 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from srcs.components.data_transformation import DataTransformation
+from srcs.components.data_transformation import DataTransformationConfig
+
+from srcs.components.model_trainer import ModelTrainer
+from srcs.components.model_trainer import ModelTrainerConfig
+
 
 @dataclass
 class DataIngestionConfig:
@@ -52,3 +58,10 @@ class DataIngestion:
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    modeltrainer.initiate_model_trainer(train_arr,test_arr)
+
